181a35db4e8497d8eea95b2236d396059a43a8f4
gautamarora04 <gautamvijayarora@gmail.com>
2023-11-03 05:09:43 +0530

    login ui

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -65,7 +65,7 @@ const Register = () => {
     //console.log(data);
   };
   return (
-    <div className="bg-gradient-to-r min-h-screen lg:min-h-screen  from-cyan-500 to-blue-500">
+    <div className="h-screen w-screen bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('/background2.jpg')" }}>
       <div className="flex justify-center py-10 ">
         <div className="bg-white w-96 h-auto border border-gray-200 rounded-md">
           <h1 className="text-center pt-4 text-[#0c2650] text-lg font-bold">
